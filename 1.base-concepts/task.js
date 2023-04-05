2e68f19b2ebddb227e06f2c4171280b082082279
Elena Yakovleva <leno4ka.1989-89@mail.ru>
2023-04-05 23:22:45 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
+  let arr=[];
+  let d = (b**2-4*a*c);
   
-  return arr;
-}
+   if (d>0){
+    let root1 = (-b + Math.sqrt(d) )/(2*a);
+    let root2 =  (-b - Math.sqrt(d) )/(2*a);
+     arr.push(root1,root2);
+     console.log(arr);
+     return arr;
+  }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+   if (d === 0){
+    let root0= -b/(2*a);
+     arr.push(root0);
+     console.log(arr);
+     return arr;
+  }else {
+     console.log("корней нет"+ arr)
+     return arr;
+  }   
 }
