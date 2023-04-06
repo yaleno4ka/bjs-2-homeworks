dd74516d3a6790155508cca28c20bfc3249a54e7
Elena Yakovleva <leno4ka.1989-89@mail.ru>
2023-04-06 11:28:09 +0300

    Реализовано  второе задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,23 +1,27 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr=[];
-  let d = (b**2-4*a*c);
+    let arr=[];
+    let d = (b**2-4*a*c);
+    
+     if (d>0){
+      let root1 = (-b + Math.sqrt(d) )/(2*a);
+      let root2 =  (-b - Math.sqrt(d) )/(2*a);
+       arr.push(root1,root2);
+       console.log(arr);
+       return arr;
+    }
   
-   if (d>0){
-    let root1 = (-b + Math.sqrt(d) )/(2*a);
-    let root2 =  (-b - Math.sqrt(d) )/(2*a);
-     arr.push(root1,root2);
-     console.log(arr);
-     return arr;
+     if (d === 0){
+      let root0= -b/(2*a);
+       arr.push(root0);
+       console.log(arr);
+       return arr;
+    }else {
+       console.log("корней нет"+ arr)
+       return arr;
+    }   
   }
 
-   if (d === 0){
-    let root0= -b/(2*a);
-     arr.push(root0);
-     console.log(arr);
-     return arr;
-  }else {
-     console.log("корней нет"+ arr)
-     return arr;
-  }   
-}
+
+
+  
