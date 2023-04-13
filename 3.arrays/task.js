7b6caa4f8154423742c37f8e7d1154e3eabf672b
Elena Yakovleva <leno4ka.1989-89@mail.ru>
2023-04-13 11:52:49 +0300

    Реализовано

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
-  
+    let result;
+ 
+    result = (arr1.length === arr2.length) && (arr1.every((elem, index) === arr2[i]));
+    
+     return result;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+    return arr.filter(item => item > 0 && item % 3 === 0).map(item => item *10);
   
 }
