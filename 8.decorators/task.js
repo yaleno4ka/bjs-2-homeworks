6bf0a4b1307e33b402c7957f6a0fe59abfd8939d
Elena Yakovleva <leno4ka.1989-89@mail.ru>
2023-04-24 22:43:59 +0300

    Реализовано

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,51 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+function wrapper(...args) {
+    const hash = args.join(','); 
+    let idx = cache.findIndex((item)=> item.hash === hash ); 
+    if (idx !== -1 ) { 
+        console.log("Из кэша: " + cache[idx].result); 
+        return "Из кэша: " + cache[idx].result;
+    }
+
+    let result = func(...args); 
+    cache.push({hash, result}) ;
+    if (cache.length > 5) { 
+      cache.shift() 
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+}
+return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
+    let timeout = null;
+
+    function wrapper(...args) {
   
+      if (timeout === null)
+      {
+        func(...args);
+        wrapper.count++;
+      }
+  
+      if(timeout) {
+        clearTimeout(timeout);
+      }
+      
+        timeout = setTimeout(() => {
+        timeout = true;
+        func(...args);
+        wrapper.count++;
+        }, delay);
+        wrapper.allCount++;
+    }
+    
+    wrapper.count = 0 
+    wrapper.allCount = 0 
+      return wrapper; 
 }
