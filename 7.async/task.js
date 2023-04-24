20522e4acbab83a00b1d60994e0aa824969c9fa3
Elena Yakovleva <leno4ka.1989-89@mail.ru>
2023-04-24 21:30:06 +0300

    Реализовано

---
+++ 7.async/task.js
@@ -0,0 +1,54 @@
+class AlarmClock { 
+    constructor() { 
+      this.alarmCollection = []; 
+      this.intervalId = null; 
+    } 
+   
+    addClock(time, callback) { 
+      if (!time || callback === undefined) { 
+        throw new Error("Отсутствуют обязательные аргументы"); 
+      } 
+      if (this.alarmCollection.some((alarm) => alarm.time === time)) { 
+        console.warn("Уже присутствует звонок на это же время"); 
+      } 
+      this.alarmCollection.push({ time, callback, canCall: true }); 
+    } 
+    removeClock(time) { 
+      this.alarmCollection = this.alarmCollection.filter( 
+        (alarm) => alarm.time !== time 
+      ); 
+    } 
+   
+    getCurrentFormattedTime() { 
+      return new Date().toLocaleTimeString([], { 
+        hour: "2-digit", 
+        minute: "2-digit", 
+      }); 
+    } 
+    start() { 
+      if (this.intervalId) { 
+        return; 
+      } 
+      this.intervalId = setInterval( 
+        () => 
+          this.alarmCollection.forEach((alarm) => { 
+            if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall) { 
+              alarm.canCall = false; 
+              alarm.callback(); 
+            } 
+          }), 
+        1000 
+      ); 
+    } 
+    stop() { 
+      clearInterval(this.intervalId); 
+      this.intervalId = null; 
+    } 
+    resetAllCalls() { 
+      this.alarmCollection.forEach((alarm) => (alarm.canCall = true)); 
+    } 
+    clearAlarms() { 
+      this.stop(); 
+      this.alarmCollection = []; 
+    } 
+  }
